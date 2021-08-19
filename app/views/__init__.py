8551fee315182508ee8a0150616576fabe942507
diklios <1061995104@qq.com>
2021-08-19 22:19:51 +0800

    解决api问题

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,7 +4,7 @@ from app.views.api import api_bp
 
 # 第一个参数是蓝图的名称，第二个参数是蓝图位置信息
 tcm_bp = Blueprint('sun', __name__)
-tcm_bp.register_blueprint(api_bp, url_prefix='/api')
+tcm_bp.register_blueprint(api_bp, url_prefix='/sun/api')
 
 
 @tcm_bp.get('/', defaults={'path': ''})
