3430b213169c3a9a2a7ae4ad06dee0537b8aed96
diklios <1061995104@qq.com>
2021-08-20 22:54:06 +0800

    变更celery的redis配置

--- app/utils/celery_handler/config.py
+++ app/utils/celery_handler/config.py
@@ -1,7 +1,7 @@
 # celery的默认设置
 
-broker_url = 'redis://localhost:49153/0'
-result_backend = 'redis://localhost:49153/1'
+broker_url = 'redis://localhost:6379/0'
+result_backend = 'redis://localhost:6379/1'
 
 task_serializer = 'json'
 result_serializer = 'json'
